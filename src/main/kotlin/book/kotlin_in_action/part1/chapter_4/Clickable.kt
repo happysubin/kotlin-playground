f4a87cd9f0466daa7f81b4667c9d4f74b010be1d
happysubin <asb0711@gmail.com>
2024-03-03 00:10:08 +0900

    코틀린 인 액션 4장

---
+++ src/main/kotlin/book/kotlin_in_action/part1/chapter_4/Clickable.kt
@@ -0,0 +1,7 @@
+package book.kotlin_in_action.part1.chapter_4
+
+interface Clickable {
+    fun click()
+
+    fun showOff() = println("I'm Clickable")
+}
---
+++ src/main/kotlin/book/kotlin_in_action/part1/chapter_4/nested/LayerV1.kt
@@ -0,0 +1,14 @@
+package book.kotlin_in_action.part1.chapter_4.nested
+
+interface Expr
+
+class Num(val value: Int): Expr
+
+class Sum(val left: Expr, val right: Expr): Expr
+
+fun eval (e: Expr): Int =
+    when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.right) + eval(e.left)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
